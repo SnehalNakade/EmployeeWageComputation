b06951df6d7ccf6801786296684af4a3eb653a98
SnehalNakade <snehalnakade98@gmail.com>
2022-04-06 10:45:18 +0530

    Added :  EmployeeWageAttendance.java

---
+++ EmployeeWage/EmployeeWageAttendance.java
@@ -0,0 +1,17 @@
+package EmployeeWage;
+/**
+ * @author sneha-Check Employee is Present or Absent
+ *
+ */
+public class EmployeeWageAttendance {
+	public static void main(String[] args) { 
+		// Constants
+		int FULL_TIME = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+		System.out.println("Employee is Abscent");
+		}
+}
